abc6540bcd0aba595a58b848893f86ae159b4709
Won Beom Kim <shepelt@gmail.com>
2021-01-25 16:57:04 +0900

    initial commit

---
+++ server/main.js
@@ -0,0 +1,49 @@
+import { Meteor } from 'meteor/meteor';
+import { _ } from 'meteor/underscore';
+import { Upbit, UpbitWs } from 'upbit-js';
+
+import '../imports/candles.js';
+
+const upbit = new Upbit();
+
+const intervalSeconds = 10;
+Meteor.startup(() => {
+	// ensure inddex on collections
+	Candles._ensureIndex({
+		market: 1,
+		timestamp: 1
+	});
+
+	fetchCandles(65);
+	setInterval(function () {
+		fetchCandles(5);
+	}, 1000 * intervalSeconds);
+});
+
+
+function fetchCandles(count) {
+	upbit.candlesMinutes({ unit: 5, market: 'KRW-BTC', count: count, to: undefined })
+		.then(value => {
+			console.log("[candles updated]")
+			_.each(value, processCandle);
+		})
+		.catch(err => {
+			console.error(err);
+		})
+}
+
+const fiveMinuteSecs = 5 * 60 * 1000;
+
+function processCandle(candle) {
+	candle.timestamp = (new Date(candle.candle_date_time_utc + "Z")).getTime();
+	var now = (new Date()).getTime();
+	var offset = now - candle.timestamp;
+	if (offset < fiveMinuteSecs) {
+		candle.closed = false;
+	} else {
+		candle.closed = true;
+	}
+	delete candle.candle_date_time_kst;
+	delete candle.unit;
+	Candles.upsert(candle.market + candle.timestamp, candle);
+}
