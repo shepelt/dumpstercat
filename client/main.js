bdbb784cd2ade210fc4fdc7a74f565275b61351e
Won Beom Kim <shepelt@gmail.com>
2021-01-26 15:26:22 +0900

    added pixijs lib and test code

--- client/main.js
+++ client/main.js
@@ -4,22 +4,58 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 import '../imports/candles.js';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.chart.onCreated(function helloOnCreated() {
+  Meteor.subscribe("candles");
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+// UI initialization
+app = null;
+stage = null;
+rectContainer = null;
+Template.chart.onRendered(function () {
+  PIXI.utils.sayHello("hello world");
+  //Create a Pixi Application
+  let dims = {
+    width: $("#chart").width(),
+    height: $("#chart").height()
+  }
+  // TODO: auto resize
 
-Template.hello.events({
+  app = new PIXI.Application({ width: dims.width, height: dims.height });
+
+  //Add the canvas that Pixi automatically created for you to the HTML document
+  $("#chart")[0].appendChild(app.view)
+
+
+  stage = new PIXI.Container();
+  rectContainer = new PIXI.Container();
+  app.stage.addChild(rectContainer);
+})
+
+// Template.hello.helpers({
+//   counter() {
+//     return Template.instance().counter.get();
+//   },
+// });
+
+Template.chart.events({
   'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+    addRect(200, 100, 10, 100, "0x9fa8a3");
   },
 });
 
-Meteor.subscribe("candles");
+addRect = function (x, y, xd, yd, color) {
+  var container = new PIXI.Container();
+  var graphics = new PIXI.Graphics();
+  graphics.lineStyle(0);
+  graphics.beginFill(color, 1.0);
+  graphics.drawRoundedRect(0, 0, xd, yd, 0);
+  graphics.endFill();
+  container.addChild(graphics);
+
+  container.x = x;
+  container.y = y;
+  rectContainer.addChild(container);
+
+  return container;
+}
