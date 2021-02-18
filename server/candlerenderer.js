969faaeab7f399a70cf1d18d63073ddb1fb4f760
Won Beom Kim <shepelt@gmail.com>
2021-02-18 14:56:45 +0900

    implmeneted basic rendering primitives

--- server/candlerenderer.js
+++ server/candlerenderer.js
@@ -11,29 +11,35 @@ getLastCandle = function (market) {
 
 
 candleDisplay = {
-    width: 5,
+    width: 10,
     wickWidth: 1,
     margin: 1,
     spacing: 2,
     positive: "0xEF5450",
     negative: "0x25A69A",
     neutral: "0x566573",
-    infowidth: 65,
+    infowidth: 130,
     background: "0x161A25",
     infobackground: "0xDDDDDD",
     textcolor: "0xFFFFFF",
-    chartwidth: 320,
-    chartheight: 240
+    chartwidth: 630,
+    chartheight: 480
 };
 
 var ctx = null;
 
-drawCandles = function (endIndex, market) {
+var basePath = process.env['METEOR_SHELL_DIR'] + '/../../../public';
+var fnt = PImage.registerFont(basePath + '/freepixel.ttf', 'Open Sans');
+fnt.load();
+
+drawCandles = function (endIndex, market, res) {
     // empty container
 
     // prepare graphics
     var img = PImage.make(candleDisplay.chartwidth, candleDisplay.chartheight);
     ctx = img.getContext('2d');
+    // background
+    addRect(0, 0, candleDisplay.chartwidth, candleDisplay.chartheight, "0x000000", 1.0);
 
     var chartWidth = candleDisplay.chartwidth - candleDisplay.infowidth;
 
@@ -99,30 +105,21 @@ drawCandles = function (endIndex, market) {
 
         // draw visual guide
         if (i == candles.length - 1) {
-            var dash = addHorizontalLine(boxHeight, canvasWidth + 1, 0.5, "0xD5402B", 0.9);
-            // rectContainer.addChild(dash);
-
-            var priceRect = addRect(canvasWidth + 2, boxHeight - 4, candleDisplay.infowidth - 1, 10, "0xD5402B");
-            // rectContainer.addChild(priceRect);
-
-            var price = addText(canvasWidth + 5, boxHeight - 4, "0x000000", "" + candle.trade_price + " KRW")
-            // rectContainer.addChild(price);
+            addHorizontalLine(boxHeight, canvasWidth + 1, 4.0, "0xD5402B", 0.9);
+            addRect(canvasWidth + 2, boxHeight - 10, candleDisplay.infowidth - 1, 20, "0xD5402B");
+            addText(canvasWidth + 5, boxHeight - 10, "0xFFFFFF", "" + candle.trade_price + " KRW")
 
         }
     }
 
-
-    ctx.lineWidth = 5;
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(100, 50);
-    ctx.stroke();
-
     // save file
-    var filePath = process.env['METEOR_SHELL_DIR'] + '/../../../public/out.png';
+    var filePath = basePath + '/out.png';
 
     PImage.encodePNGToStream(img, fs.createWriteStream(filePath)).then(() => {
         console.log("wrote out the png file to out.png", filePath);
+        if (res) {
+            res(filePath);
+        }
     }).catch((e) => {
         console.log("there was an error writing");
     });
@@ -143,15 +140,66 @@ candleScreenToOrder = function (x) {
     return index;
 }
 
+// rendering implementation
 addHorizontalLine = function (y, width, linethickness, linecolor, alpha) {
-    console.log("[HORIZONTAL LINE]", y, width)
+    ctx.lineWidth = linethickness;
 
+    var gap = 10;
+    var alternate = false;
+    for (var x = 0; x < width; x += gap) {
+        if (alternate) {
+            alternate = false;
+            ctx.strokeStyle = hexToRgbA(linecolor, alpha);
+        } else {
+            alternate = true;
+            ctx.strokeStyle = hexToRgbA(linecolor, 0);
+        }
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(x + gap, y);
+        ctx.stroke();
+    }
 }
 
 addRect = function (x, y, xd, yd, color, alpha, text, textcolor) {
-    console.log("[RECT]", x, y, xd, yd)
+    if (alpha == undefined)
+        alpha = 1.0;
+
+    // normalized coordinates
+    if (xd < 0) {
+        x = x + xd;
+        xd = Math.abs(xd);
+    }
+    if (yd < 0) {
+        y = y + yd;
+        yd = Math.abs(yd);
+    }
+    ctx.fillStyle = hexToRgbA(color, alpha);
+    ctx.fillRect(x, y, xd, yd);
+
+    if (text) {
+        ctx.fillStyle = hexToRgbA(textcolor, 1.0);
+        ctx.font = "20pt 'Open Sans'";
+        ctx.fillText(text, x - 5, y - 5);
+    }
 }
 
 addText = function (x, y, color, text) {
-    console.log("[TEXT]", x, y, text)
+    ctx.fillStyle = hexToRgbA(color, 1.0);
+    ctx.font = "20pt 'Open Sans'";
+    ctx.fillText(text, x, y + 15);
+}
+
+function hexToRgbA(hex, alpha) {
+    var c;
+    hex = hex.replace("0x", "#");
+    if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
+        c = hex.substring(1).split('');
+        if (c.length == 3) {
+            c = [c[0], c[0], c[1], c[1], c[2], c[2]];
+        }
+        c = '0x' + c.join('');
+        return 'rgba(' + [(c >> 16) & 255, (c >> 8) & 255, c & 255].join(',') + ',' + alpha + ')';
+    }
+    throw new Error('Bad Hex');
 }
